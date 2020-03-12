d94ba4259c0dfc9f30e9ecf1f15deca007b628ab
rado9779 <radostin9779@gmail.com>
2020-03-12 12:27:19 +0200

    Add ForumController

---
+++ Web/Cinephile.Web/Controllers/ForumController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinephile.Web.Controllers
+{
+    public class ForumController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        public IActionResult Rules()
+        {
+            return this.View();
+        }
+    }
+}
