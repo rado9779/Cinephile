c851062be8f71026828610e2681a9c17a84defca
rado9779 <radostin9779@gmail.com>
2020-03-12 14:54:16 +0200

    Add Database Models

---
+++ Data/Cinephile.Data.Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿namespace Cinephile.Data.Models
+{
+    using Cinephile.Data.Common.Models;
+
+    public class Comment : BaseDeletableModel<int>
+    {
+        public int PostId { get; set; }
+
+        public virtual Post Post { get; set; }
+
+        public string Content { get; set; }
+
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
