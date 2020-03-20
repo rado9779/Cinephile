b5d2dcdb7221f04f20246969b5a9ca1d060241ab
rado9779 <radostin9779@gmail.com>
2020-03-20 14:54:40 +0200

    Add Category Seeding

---
+++ Data/Cinephile.Data/Seeding/CategoriesSeeder.cs
@@ -0,0 +1,33 @@
+﻿namespace Cinephile.Data.Seeding
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Cinephile.Data.Models;
+
+    public class CategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Categories.Any())
+            {
+                return;
+            }
+
+            var categories = new List<string>() { "Movies", "TV-Series", "Actors" };
+
+            foreach (var category in categories)
+            {
+                await dbContext.Categories.AddAsync(new Category
+                {
+                    Name = category,
+                    Title = category,
+                    Description = category,
+                    ImageUrl = category,
+                });
+            }
+        }
+    }
+}
