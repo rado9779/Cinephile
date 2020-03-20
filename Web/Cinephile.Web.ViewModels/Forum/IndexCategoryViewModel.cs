4cb7d30ac509f47019f7fdd2a6cbfc40bfdd93d8
rado9779 <radostin9779@gmail.com>
2020-03-20 14:58:11 +0200

    Add Category Service

---
+++ Web/Cinephile.Web.ViewModels/Forum/IndexCategoryViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Cinephile.Web.ViewModels.Forum
+{
+    using Cinephile.Data.Models;
+    using Cinephile.Services.Mapping;
+
+    public class IndexCategoryViewModel : IMapFrom<Category>
+    {
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int PostsCount { get; set; }
+
+        public string Url => $"/{this.Name.Replace(' ', '-')}";
+    }
+}
---
+++ Web/Cinephile.Web.ViewModels/Forum/IndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Cinephile.Web.ViewModels.Forum
+{
+    using System.Collections.Generic;
+
+    public class IndexViewModel
+    {
+        public IEnumerable<IndexCategoryViewModel> Categories { get; set; }
+    }
+}
