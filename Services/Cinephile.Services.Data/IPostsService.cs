11042fb3f841f1d2579d8c6becfe0fd763681ece
rado9779 <radostin9779@gmail.com>
2020-03-26 17:12:48 +0200

    Upload PostsServices

---
+++ Services/Cinephile.Services.Data/IPostsService.cs
@@ -0,0 +1,11 @@
+﻿namespace Cinephile.Services.Data
+{
+    using System.Threading.Tasks;
+
+    public interface IPostsService
+    {
+        Task<int> Create(string title, string content, int categoryId, string userId);
+
+        T GetById<T>(int id);
+    }
+}
---
+++ Services/Cinephile.Services.Data/PostsService.cs
@@ -0,0 +1,41 @@
+﻿namespace Cinephile.Services.Data
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Cinephile.Data.Common.Repositories;
+    using Cinephile.Data.Models;
+    using Cinephile.Services.Mapping;
+
+    public class PostsService : IPostsService
+    {
+        private readonly IDeletableEntityRepository<Post> postsRepository;
+
+        public PostsService(IDeletableEntityRepository<Post> postsRepository)
+        {
+            this.postsRepository = postsRepository;
+        }
+
+        public async Task<int> Create(string title, string content, int categoryId, string userId)
+        {
+            var post = new Post
+            {
+                Title = title,
+                Content = content,
+                CategoryId = categoryId,
+            };
+
+            await this.postsRepository.AddAsync(post);
+            await this.postsRepository.SaveChangesAsync();
+
+            return post.Id;
+        }
+
+        public T GetById<T>(int id)
+        {
+            var post = this.postsRepository.All().Where(x => x.Id == id)
+                .To<T>().FirstOrDefault();
+            return post;
+        }
+    }
+}
