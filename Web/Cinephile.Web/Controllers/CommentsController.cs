143e21780767035dcb89cbf49f8df762dafb635b
rado9779 <radostin9779@gmail.com>
2020-04-08 12:34:10 +0300

    Add CommentsController

---
+++ Web/Cinephile.Web/Controllers/CommentsController.cs
@@ -0,0 +1,31 @@
+﻿namespace Cinephile.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Cinephile.Data.Models;
+    using Cinephile.Services.Data;
+    using Cinephile.Web.ViewModels.Comments;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class CommentsController : Controller
+    {
+        private readonly ICommentsService commentsService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public CommentsController(
+            ICommentsService commentsService,
+            UserManager<ApplicationUser> userManager)
+        {
+            this.commentsService = commentsService;
+            this.userManager = userManager;
+        }
+
+        public async Task<IActionResult> Create(CommentInputViewModel input)
+        {
+            var userId = this.userManager.GetUserId(this.User);
+            await this.commentsService.Create(input.PostId, userId, input.Content);
+            return this.RedirectToAction("ById", "Posts", new { id = input.PostId });
+        }
+    }
+}
