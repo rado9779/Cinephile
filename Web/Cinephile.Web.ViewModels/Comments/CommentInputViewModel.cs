0ac7b28d3e86332b7788544003998b7cc5b85f2d
rado9779 <radostin9779@gmail.com>
2020-04-08 12:33:17 +0300

    Add CommentInputViewModel

---
+++ Web/Cinephile.Web.ViewModels/Comments/CommentInputViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Cinephile.Web.ViewModels.Comments
+{
+    using Cinephile.Data.Models;
+    using Cinephile.Services.Mapping;
+
+    public class CommentInputViewModel : IMapTo<Comment>
+    {
+        public int PostId { get; set; }
+
+        public string Content { get; set; }
+    }
+}
