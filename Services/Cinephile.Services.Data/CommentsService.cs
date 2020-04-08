7939dd2779150864d1affa0e1393633b5fac0c89
rado9779 <radostin9779@gmail.com>
2020-04-08 12:35:03 +0300

    Add ICommentsService and CommentsService

---
+++ Services/Cinephile.Services.Data/CommentsService.cs
@@ -0,0 +1,30 @@
+﻿namespace Cinephile.Services.Data
+{
+    using System.Threading.Tasks;
+
+    using Cinephile.Data.Common.Repositories;
+    using Cinephile.Data.Models;
+
+    public class CommentsService : ICommentsService
+    {
+        private readonly IDeletableEntityRepository<Comment> commentsRepository;
+
+        public CommentsService(IDeletableEntityRepository<Comment> commentsRepository)
+        {
+            this.commentsRepository = commentsRepository;
+        }
+
+        public async Task Create(int postId, string userId, string content)
+        {
+            var comment = new Comment()
+            {
+                PostId = postId,
+                UserId = userId,
+                Content = content,
+            };
+
+            await this.commentsRepository.AddAsync(comment);
+            await this.commentsRepository.SaveChangesAsync();
+        }
+    }
+}
