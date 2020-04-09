5257b8fb20720e0fbd46c5d2aced9fcd6f1c9a7b
rado9779 <radostin9779@gmail.com>
2020-04-09 10:59:07 +0300

    Implement GetUserByName Method

--- Services/Cinephile.Services.Data/UsersService.cs
+++ Services/Cinephile.Services.Data/UsersService.cs
@@ -10,19 +10,33 @@
     public class UsersService : IUsersService
     {
         private readonly IDeletableEntityRepository<Post> postsRepository;
+        private readonly IDeletableEntityRepository<ApplicationUser> usersRepository;
 
-        public UsersService(IDeletableEntityRepository<Post> postsRepository)
+        public UsersService(
+            IDeletableEntityRepository<Post> postsRepository,
+            IDeletableEntityRepository<ApplicationUser> usersRepository)
         {
             this.postsRepository = postsRepository;
+            this.usersRepository = usersRepository;
         }
 
-        public IEnumerable<T> GetAllUserPosts<T>(string userId)
+        public IEnumerable<T> GetAllUserPosts<T>(string username)
         {
             IQueryable<Post> query = this.postsRepository
                  .All()
-                 .Where(x => x.UserId == userId);
+                 .Where(x => x.UserId != null)
+                 .Where(x => x.User.UserName == username);
 
             return query.To<T>().ToList();
+        }
+
+        public T GetUserByName<T>(string username)
+        {
+            var user = this.usersRepository.All()
+                  .Where(x => x.UserName == username)
+                  .To<T>().FirstOrDefault();
+
+            return user;
         }
     }
 }
