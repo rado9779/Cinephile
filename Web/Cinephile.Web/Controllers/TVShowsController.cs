7deaa31c8288ce50756e2f4e962b46870b1303b0
rado9779 <radostin9779@gmail.com>
2020-04-22 11:56:46 +0300

    Implement view model in action

--- Web/Cinephile.Web/Controllers/TVShowsController.cs
+++ Web/Cinephile.Web/Controllers/TVShowsController.cs
@@ -1,16 +1,25 @@
 ﻿namespace Cinephile.Web.Controllers
 {
+    using Cinephile.Services.Data;
     using Cinephile.Web.ViewModels.TVShows;
     using Microsoft.AspNetCore.Mvc;
 
     public class TVShowsController : Controller
     {
+        private readonly ITVShowsService tvshowsService;
+
+        public TVShowsController(ITVShowsService tvshowsService)
+        {
+            this.tvshowsService = tvshowsService;
+        }
+
         public IActionResult TVShowsIndex()
         {
-            var viewModel = new AllTVShowsViewModel();
+            var viewModel = new AllTVShowsViewModel()
             {
-
-            }
+                TVShows = this.tvshowsService
+                .GetAll<TVShowViewModel>(),
+            };
             return this.View(viewModel);
         }
 
