aba6d7c02522b43c128454f9266755aa8cd5af3e
rado9779 <radostin9779@gmail.com>
2020-04-25 21:18:38 +0300

    Add Tests for CommentsService

---
+++ Tests/Cinephile.Services.Data.Tests/CommentsServiceTests.cs
@@ -0,0 +1,44 @@
+﻿namespace Cinephile.Services.Data.Tests
+{
+    using System.Threading.Tasks;
+    using Cinephile.Data;
+    using Cinephile.Data.Models;
+    using Cinephile.Data.Repositories;
+    using Cinephile.Services.Data.Tests.Common;
+    using Xunit;
+
+    public class CommentsServiceTests
+    {
+
+        [Fact]
+        public async Task GetById_WithCorrectData_ShouldReturnCorrectResult()
+        {
+            var dbContext = ApplicationDbContextCreatorInMemory.InitializeContext();
+
+            await this.CreateTestComments(dbContext);
+
+            var repository = new EfDeletableEntityRepository<Comment>(dbContext);
+            var service = new CommentsService(repository);
+
+            var result = service.GetCountByPostId(1);
+
+            Assert.Equal(2, result);
+        }
+
+        private async Task CreateTestComments(ApplicationDbContext dbContext)
+        {
+            dbContext.Comments.Add(new Comment
+            {
+                Id = 1,
+                PostId = 1,
+            });
+            dbContext.Comments.Add(new Comment
+            {
+                Id = 2,
+                PostId = 1,
+            });
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Tests/Cinephile.Services.Data.Tests/Common/ApplicationDbContextCreatorInMemory.cs
@@ -0,0 +1,19 @@
+﻿namespace Cinephile.Services.Data.Tests.Common
+{
+    using System;
+
+    using Cinephile.Data;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContextCreatorInMemory
+    {
+        public static ApplicationDbContext InitializeContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+               .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+               .Options;
+
+            return new ApplicationDbContext(options);
+        }
+    }
+}
